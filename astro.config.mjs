22fef485480e24b2fbb48cea49360bba5a689a73
Jose Luis <88117946+LuguiM@users.noreply.github.com>
2024-06-28 14:56:29 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://luguim.github.io/Portafolio-plantilla',
   integrations: [tailwind()]
 
 });
